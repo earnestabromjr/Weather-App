0050c5dc5353f3bfdeda069d483184b11a48f90d
earnestabromjr <abromearnest@gmail.com>
2025-11-15 23:06:05 -0600

    Started function to display weather conditions in display module
    At this point the function is untested

---
+++ src/display.js
@@ -0,0 +1,8 @@
+import { showConditions } from "./weather";
+
+export const displayWeather = () => {
+	const appMain = document.querySelector("#app");
+	const displayWeather = document.querySelector("#weather");
+
+	displayWeather.textContent = { ...showConditions() };
+};

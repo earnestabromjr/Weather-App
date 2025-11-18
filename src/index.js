040d0b45438160ee39258716732a527b0d9b3d6f
earnestabromjr <abromearnest@gmail.com>
2025-11-17 21:14:04 -0600

    Changed index module
    Getting webpack error when running on void

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ import { showConditions } from "./display";
 import { parseLocation } from "./utils";
 
 const baseUrl =
-  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
+	"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
 const apiKey = "NXM6AE9X7YT4X22T94BU7547V";
 const searchForm = document.getElementById("search-form");
 const searchButton = document.querySelector("button");
 
 searchButton.addEventListener("click", () => {
-  const searchInput = document.getElementById("search-input");
-  const city = searchInput.value;
-  const searchCity = 
-  console.log(searchCity);
-  console.log(city);
+	const searchInput = document.getElementById("search-input");
+	const city = searchInput.value;
+	const searchCity = parseLocation(city);
+	displayWeather(searchCity);
+	console.log(searchCity);
+	console.log(city);
 });
 
 // const requestUrl = `${baseUrl}/${city}?unitGroup=us&key=${apiKey}&contentType=json&startDate=today`;

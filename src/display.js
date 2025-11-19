bb1569b052abf889006cc50aa8d3f879e42c3433
earnestabromjr <abromearnest@gmail.com>
2025-11-18 18:08:23 -0600

    Modified the display module to properly display the object returned from showconditions

--- src/display.js
+++ src/display.js
@@ -3,6 +3,11 @@ import { showConditions } from "./weather";
 export const displayWeather = () => {
 	const appMain = document.querySelector("#app");
 	const displayWeather = document.querySelector("#weather");
-	displayWeather.textContent = { ...showConditions() };
+	const conditions = showConditions();
+	for (const [key, value] of Object.entries(conditions)) {
+		const li = document.createElement("li");
+		li.textContent = `${key}: ${value}`;
+		displayWeather.appendChild(li);
+	}
 	appMain.appendChild(displayWeather);
 };
